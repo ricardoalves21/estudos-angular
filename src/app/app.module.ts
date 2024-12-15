dfddbde1ec0016d2a964985cb451c49f93fb12aa
ricardoalves21 <dev.rical21@gmail.com>
2024-12-15 16:38:41 -0300

    Compartilhando dados entre os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { ParentDataComponent } from './parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule,
