83e2dacd1f75979f49c0033460495cc4c5eff335
ricardoalves21 <dev.rical21@gmail.com>
2024-12-17 21:38:41 -0300

    Aprendendo sobre eventos

--- src/app/components/directives/directives.component.ts
+++ src/app/components/directives/directives.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
 })
+
 export class DirectivesComponent implements OnInit {
 
   size = 40;
--- src/app/components/if-render/if-render.component.ts
+++ src/app/components/if-render/if-render.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IfRenderComponent implements OnInit {
 
-  canShow: boolean = false;
+  canShow: boolean = true;
 
-  name: string = 'Ricardo';
+  name: string = 'Matheus';
 
   constructor() { }
 
